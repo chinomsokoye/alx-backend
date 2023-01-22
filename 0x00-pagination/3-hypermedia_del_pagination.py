8c54f6bec965344f77d37030d9be24b231907409
nomso <chinomsokoye@gmail.com>
2023-01-22 21:20:44 +0000

    Write Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -17,9 +17,50 @@ class Server:
         self.__dataset = None
         self.__indexed_dataset = None
 
-        return {
-            "index": index,
-            "next_index": i,
-            "page_size": page_size,
-            "data": d
-            }
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+                }
+            return self.__indexed_dataset
+
+        def get_hyper_index(self, index: int = None,
+                            page_size: int = 10) -> Dict:
+            """Method get_hyper_index
+            """
+            dataset = self.indexed_dataset()
+            data_length = len(dataset)
+            assert 0 <= index < data_length
+            res = {}
+            data = []
+            res['index'] = index
+            for i in range(page_size):
+                while True:
+                    current = dataset.get(index)
+                    index += 1
+                    if current is not None:
+                        break
+                data.append(current)
+
+            res['data'] = data
+            res['page_size'] = len(data)
+            if dataset.get(index):
+                res['next_index'] = index
+            else:
+                res['next_index'] = None
+            return res
