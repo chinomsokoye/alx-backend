c8320abe7415998728fef4d7085deaceec61fa45
nomso <chinomsokoye@gmail.com>
2023-01-22 20:58:09 +0000

    Write Deletion-resilient hypermedia pagination

---
+++ 0x00-pagination/3-main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+Server = __import__('3-hypermedia_del_pagination').Server
+
+server = Server()
+
+server.indexed_dataset()
+
+try:
+    server.get_hyper_index(300000, 100)
+except AssertionError:
+    print("AssertionError raised when out of range")
+
+
+index = 3
+page_size = 2
+
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 1- request first index
+res = server.get_hyper_index(index, page_size)
+print(res)
+
+# 2- request next index
+print(server.get_hyper_index(res.get('next_index'), page_size))
+
+# 3- remove the first index
+del server._Server__indexed_dataset[res.get('index')]
+print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+# 4- request again initial index -> first data retrieves not same as first
+print(server.get_hyper_index(index, page_size))
+
+# 5- request again initial next index -> same data as the request 2-
+print(server.get_hyper_index(res.get('next_index'), page_size))
