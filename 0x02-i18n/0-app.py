63ea6f3b5d15b357d7c07c1109a4d2f72cc1409e
nomso <chinomsokoye@gmail.com>
2023-01-31 22:51:03 +0000

    Setup basic flask app, simply outputs 0-index.html file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
-def hello_world():
+def hello_world() -> str:
     ''' Output templates '''
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
